5979b6770652268d7f36640755aa95b5938f448e
evanknep <knepx001@umn.edu>
2024-02-04 18:25:38 -0600

    Analysis steps added

--- R/week3.R
+++ R/week3.R
@@ -9,3 +9,10 @@ clean_df <- subset(raw_df, grepl("2017-06", raw_df$timeStart) == FALSE)
 clean_df <- subset(clean_df, q6==1)
 
 #Analysis
+clean_df$timeSpent <- abs(difftime(clean_df$timeStart, clean_df$timeEnd))
+hist(as.integer(clean_df$timeSpent), xlab = 'Time Spent', main = 'Frequencies of Time Spent')
+frequency_tables_list <- lapply(clean_df[5:14], table)
+lapply(frequency_tables_list, barplot)
+sum(clean_df$q1 >= clean_df$q2 & clean_df$q2 != clean_df$q3)
+for (i in frequency_tables_list){
+  barplot(i)}
